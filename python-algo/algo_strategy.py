cd0b7cd8dc541d608e8b1fa4c651bf00accd3ba9
ChaoticZoo <52227193+ChaoticZoo@users.noreply.github.com>
2023-03-25 18:19:30 -0400

    Update algo_strategy.py

--- python-algo/algo_strategy.py
+++ python-algo/algo_strategy.py
@@ -270,7 +270,8 @@ class AlgoStrategy(gamelib.AlgoCore):
                 self.scored_on_locations.append(location)
                 gamelib.debug_write("All locations: {}".format(self.scored_on_locations))
 
-    game_state = gamelib.GameState(self.config, turn_string, tiles)
+        if state["turnInfo"][2] == 0:
+            game_state = gamelib.GameState(self.config, turn_string, tiles, True)
 
 
 if __name__ == "__main__":
